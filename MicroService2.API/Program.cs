bf433ff33f70c9b2361b268d851d99bdac6845f0
Kamran <311.kamran@gmail.com>
2023-08-14 18:03:17 +0400

    Docker Compose --> Up ( build, recreate, start )                                     Down ( stop, remove )

--- MicroService2.API/Program.cs
+++ MicroService2.API/Program.cs
@@ -22,5 +22,10 @@ app.Run();
 
 
 
-// docker compose stop  -- > calisan containeri dayandiririq 
-// docker compose rm    -- > calismayan containerleri silmek ucun istifade edirik
+// docker compose stop  -- > calisan containeri dayandiririq.
+// docker compose rm    -- > calismayan containerleri silmek ucun istifade edirik.
+// docker compose up    -- > imagelarimizi 0dan build edir,Containerleri sifirdan olusturur 
+//                           eyni zamanda containeri ise baslatir (attach formasinda).
+// docker compose down  -- > Containerleri dayandirir ve ardiycan silir, eyni zamanda
+//                           up ile bir yerde yaranan Volumelarda varsa onlarida silir.
+// docker compose up -d -- > Containerleri de-attach olaraq yaradir.
