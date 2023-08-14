f6123c4c41681cc9978c98d9904208fc06bfc8c3
Kamran <311.kamran@gmail.com>
2023-08-14 15:28:27 +0400

    Add project files.

---
+++ MicroService1.API/Program.cs
@@ -0,0 +1,30 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline. Ve ancaq development ortamda olmasi ucundu,
+// production ortamda swagger gorunmur
+
+// if (app.Environment.IsDevelopment() )
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+
+//// Productionda da gorunmeyin isteyirikse swaggerin bele yazmaliyiq
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
