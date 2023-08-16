34f1f0dbfb0c3fecbbe7aaac894443b8bd63f7cf
Kamran <311.kamran@gmail.com>
2023-08-16 11:14:48 +0400

    Sql Server -- > Sql Server Service
    Burada local projectimiz ile dockerde olan SqlServer Containeri ile elaqe yaratdiq.

---
+++ MicroService1.API/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using MicroService1.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroService1.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    private readonly AppDbContext _dbContext;
+
+    public ProductsController(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    [HttpPost]
+    public IActionResult Save()
+    {
+        _dbContext.Products.Add(new Product() { Name = "Pen 1" });
+        _dbContext.SaveChanges();
+
+        return Ok();
+    }
+}
